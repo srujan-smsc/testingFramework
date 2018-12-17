16983ebb346441c4fd2ca5675d7b00ba6781309c
SMSCountry Networks Pvt. Ltd <smscountrynetworkspvt.ltd.@SMSCountrys-iMac.local>
2018-12-17 15:27:22 +0530

    Initial Commit

---
+++ testingFramework/testingFramework.h
@@ -0,0 +1,19 @@
+//
+//  testingFramework.h
+//  testingFramework
+//
+//  Created by SMSCountry Networks Pvt. Ltd. on 17/12/18.
+//  Copyright © 2018 SMSCountry Networks Pvt. Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for testingFramework.
+FOUNDATION_EXPORT double testingFrameworkVersionNumber;
+
+//! Project version string for testingFramework.
+FOUNDATION_EXPORT const unsigned char testingFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <testingFramework/PublicHeader.h>
+
+
