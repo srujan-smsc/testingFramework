596e38a90943dd68674bacd9096a08ee1d072df1
SMSCountry Networks Pvt. Ltd <smscountrynetworkspvt.ltd.@SMSCountrys-iMac.local>
2018-12-17 15:32:38 +0530

    updated pod specs

---
+++ testingFramework/testVC.h
@@ -0,0 +1,17 @@
+//
+//  testVC.h
+//  testingFramework
+//
+//  Created by SMSCountry Networks Pvt. Ltd. on 17/12/18.
+//  Copyright © 2018 SMSCountry Networks Pvt. Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface testVC : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ testingFramework/testVC.m
@@ -0,0 +1,32 @@
+//
+//  testVC.m
+//  testingFramework
+//
+//  Created by SMSCountry Networks Pvt. Ltd. on 17/12/18.
+//  Copyright © 2018 SMSCountry Networks Pvt. Ltd. All rights reserved.
+//
+
+#import "testVC.h"
+
+@interface testVC ()
+
+@end
+
+@implementation testVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
